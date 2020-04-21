192a3321711e9aac5234bcfb51f3067e33bc58ff
tdkmp4n4 <davidalvarezrobles@gmail.com>
2020-04-21 17:50:05 -0400

    Bugfixes

--- Utils/PortScan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "[*] Portscan started"
-host=$1
-for port in $(seq 1 65535);
-do
-	timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null && echo "[+] Port $port is open"
-done
-echo "[*] Portscan finished"
